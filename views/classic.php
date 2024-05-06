4ad64b7d419b62087b2b34c1e80cb2b3f88b92b2
g5pq352 <g5pq352@gmail.com>
2024-05-06 19:07:15 +0800

    Update classic.php

--- views/classic.php
+++ views/classic.php
@@ -523,7 +523,7 @@ class classicSlider {
 			width: "100vw",
 			ease: Power4.easeInOut,
 			onComplete: () =>{
-				var newUrl = 'http://127.0.0.1/apex-group/classic/' + `classic${this._index}`;
+				var newUrl = '<?= $base_rul ?>/classic/' + `classic${this._index}`;
 				location.href = newUrl
 				// window.history.pushState(null, null, newUrl);
 				// location.reload();
