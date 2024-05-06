7649fec537f254ef3ba8050b2d86b3cfe0975781
g5pq352 <g5pq352@gmail.com>
2024-05-06 18:56:29 +0800

    Update index.php

--- index.php
+++ index.php
@@ -13,12 +13,7 @@ $app->hook('slim.before', function () use ($app) {
   $host = $req->getHost();
   $port = $_SERVER['SERVER_PORT'] != 80 && $_SERVER['SERVER_PORT'] != 443 ? ':' . $_SERVER['SERVER_PORT'] : '';
   $root = $req->getRootUri();
-
-  if($_SERVER['HTTP_HOST'] == "127.0.0.1" || $_SERVER['HTTP_HOST'] == "localhost"){
-    $baseurl = (isset($_SERVER['HTTPS']) ? "https://" : "http://") . $host . $port . $root;
-  }else{
-    $baseurl = "https://". $host . $root;
-  }
+  $baseurl = (isset($_SERVER['HTTPS']) ? "https://" : "http://") . $host . $port . $root;
   
   $app->view()->appendData(array('baseurl' => $baseurl));
 });
